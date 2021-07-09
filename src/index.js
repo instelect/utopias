310fb6e3694d797f9752d2fb5bcdc93ba6df97bd
Marcelo <instalarelectricidad@gmail.com>
2021-07-09 01:14:03 -0300

    primer subida a servidor

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const { Console } = require('console');
+const { renderFile } = require('ejs');
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// settings
+app.set('port', 4068);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+// middleware
+
+
+// routes
+
+app.use(require('./routes/'));
+
+// Static files
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+// listening the server
+app.listen(app.get('port'), () => {
+    console.log('Server on port', app.get('port'));
+});
---
+++ src/routes/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    //res.sendFile(path.join(__dirname, 'views/index.html'));
+    res.render('index.html', {title: 'Utopias'});
+});
+
+    router.get('/contact', (req, res) => {
+     res.render('contact.html', {title: 'Contact Page'});   
+});
+
+module.exports = router;
