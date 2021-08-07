58f90680e9d2777a0bdeb03d57a3d979026b5b7d
Marcelo <instalarelectricidad@gmail.com>
2021-08-06 21:22:01 -0300

    modifico packagr.json

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-const port = process.env.PORT || 3000;                  
+//const port = process.env.PORT || 3000;                  
 // settings
 //app.set('port', 3000);
 //app.set('views', path.join(__dirname, 'views'));
